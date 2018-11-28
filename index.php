1fc8bbbc9e79e8efd0056edbbafa1c4f7546f8d5
root <emploi.neveu@gmail.com>
2018-11-28 12:00:51 +0100

    modif

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Exercice 6</title>
-  </head>
-  <body>
-    <?php
-    if (empty($_POST['prenom']) || empty($_POST['nom']) || empty($_POST['gender'])){
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 6</title>
+</head>
+<body>
+  <?php
+    if (empty($_GET['prenom']) || empty($_GET['nom'])){
+    ?>
+  <form method="GET" action="index.php">
+    <select name="gender">
+      <option value="Homme">Homme</option>
+      <option value="Femme">Femme</option>
+    </select>
+    <div>
+      <label for="lastname">Nom :</label>
+      <input type="text" name="nom" />
+    </div>
+    <div>
+      <label for="firstname">Prénom :</label>
+      <input type="text" name="prenom" />
+    </div>
+    <div class="button">
+      <button type="submit">Envoyer</button>
+    </div>
+  </form>
+  <?php
+    }
+    else{
+        echo 'Bonjour '. ' '.$_GET['prenom']. ' ' .$_GET['nom'].' '. 'tu es un(e)' . ' ' . $_GET['gender'];
     }
     ?>
-    <form action="index.php" method="post">
-    <select name="gender">
-      <option value="Mr">Mr</option>
-      <option value="Mme">Mme</option>
-    </select>
-    <p>prénom<input type="text" name="prenom" value=""></p>
-    <p>nom<input type="text" name="nom" value=""></p>
-    <button type="submit" name="button">Valider</button>
-    </form>
-    <?php
-    echo $_POST['gender'] . ' ' . $_POST['prenom'] . ' ' . $_POST['nom'];
-    ?>
-  </body>
-</html>
+</body>
